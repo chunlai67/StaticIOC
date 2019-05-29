e2a22954410d907ab354a82f80eb56223cc63dbf
zhouchunlai <124033543@qq.com>
2019-05-29 23:34:32 +0800

    new proj

---
+++ cmake-build-debug/CMakeFiles/ioc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ioc.dir/main.cpp.o"
+  "ioc.pdb"
+  "ioc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ioc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cassert>
+#include <type_traits>
+
+enum TestEnum
+{
+    E_0,
+    E_1,
+    E_2
+};
+
+template <typename C0, typename C1, typename C2>
+class HanlderManager
+{
+public:
+    
+};
+
+int main(int argc, char* argv[])
+{
+
+
+    return 0;
+}
